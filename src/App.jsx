b8475c2a16787b0e6d88f954fd9281293b88dc07
dinesh suthar <dineshsutharr427@gmail.com>
2025-09-02 13:31:07 +0530

    change resposivenes

--- src/App.jsx
+++ src/App.jsx
@@ -80,7 +80,7 @@ function Intro() {
         <h1>Welcome! I’m Dinesh suthar, a Web Developer</h1>
         <p className="lead">I build fast, accessible web apps with React, javascript, Node.js, and modern CSS. I specialize in performance, clean architectures, and delightful UX.</p>
       </div>
-      <img src="../public/image.jpg" alt="A person coding on a laptop in a dark room." className="intro-image" />
+      <img src="/image.jpg" alt="A person coding on a laptop in a dark room." className="intro-image" />
     </section>
   )
 }
